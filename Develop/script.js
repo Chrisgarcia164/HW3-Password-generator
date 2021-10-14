cf253f0d5d4e60ee0a33e8c596f75bb6e748341c
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-14 18:00:30 -0400

    Added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+var firstPrompt = confirm("Would you like to include uppercase letters?")
+var secondPrompt = confirm("Would you like to include lowercase letters")
+var thirdPrompt = confirm("Would you like to include special characters?")
+var fourthPrompt = confirm("Would you like to include numbers?")
+var fifthPrompt = prompt("Please choose how many characters you would like in your password. \nPlease note that character length must be between 8 and 128.")
+
+
+console.log(fifthPrompt)
+if (firstPrompt && secondPrompt && thirdPrompt && fourthPrompt) {
+  console.log("It worked")
+}
 
 // Write password to the #password input
 function writePassword() {
